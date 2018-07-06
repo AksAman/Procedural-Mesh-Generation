b52e77e85070bba986bc5fa27acf5bf2a7f221bf
AksAman <AksAman@github.com>
2018-07-06 21:45:57 +0530

    Procedural plane generated

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -1,39 +1,75 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+//[ExecuteInEditMode]
 public class GridGenerator : MonoBehaviour {
 	public int xSize, ySize;
 	private Vector3[] vertices;
+	private Vector2[] uv;
+	private Mesh mesh;
+	int[] triangles;
 
 	void Awake()
 	{
-		StartCoroutine (Generate ());
+//		StartCoroutine (Generate ());
+		Generate ();
 	}
-
-	IEnumerator Generate()
+//	void OnValidate()
+//	{
+//		Generate ();
+//	}
+	void Generate()
 	{
-		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+//		mesh = new Mesh ();
+		GetComponent<MeshFilter> ().mesh = mesh = new Mesh ();
+		mesh.name = "Procedural Grid";
 
-		for (int i=0 ,x = 0; x <= xSize; x++) {
-			for (int y = 0; y <= ySize; y++, i++) {
+		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		uv = new Vector2[vertices.Length];
+		mesh.Clear ();
+
+
+		for (int i=0 , y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++, i++) {
 				vertices [i] = new Vector3 (x, y, 0);
-				yield return new WaitForSeconds (0.05f);
+				uv [i] = new Vector2 ((float)x / xSize, (float)y / ySize);
+
 			}
 		}
+
+		mesh.vertices = vertices;
+		mesh.uv = uv;
+
+		triangles = new int[xSize * 6 * ySize];
+//		Mesh Generation in bottom row
+		// ti - triangle index, vi- vertex index
+		for (int ti = 0, vi =0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti+=6, vi++) {
+				triangles [ti] = vi;
+				triangles [ti + 1] = triangles [ti + 4] = xSize + 1+ vi;
+				triangles [ti + 2] = triangles [ti + 3] =1 + vi;
+				triangles [ti + 5] = xSize + 2 + vi;
+
+			}
+		}
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+
+
 	}
 
-	void OnDrawGizmos()
-	{
-		if(vertices == null)
-		{
-			return;
-		}
-
-		Gizmos.color = Color.white;
-		for (int i = 0; i < vertices.Length; i++) {
-			Gizmos.DrawSphere (vertices [i], 0.1f);
-		}
-
-	}
+//	void OnDrawGizmos()
+//	{
+//		if(vertices == null)
+//		{
+//			return;
+//		}
+//
+//		Gizmos.color = Color.white;
+//		for (int i = 0; i < vertices.Length; i++) {
+//			Gizmos.DrawSphere (vertices [i], 0.1f);
+//		}
+//
+//	}
 
 }
